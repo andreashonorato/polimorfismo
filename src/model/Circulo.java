b95e47c5e5624b89307fa9c031975dfb5000d9ad
andre <andre@10.146.192.191>
2025-05-05 11:11:23 -0300

    Polimorfismo

---
+++ src/model/Circulo.java
@@ -0,0 +1,21 @@
+
+package model;
+
+
+public class Circulo extends FiguraGeometrica {
+    private double raio;
+
+    public Circulo() {
+    }
+    public Circulo(String cor,double raio) {
+        super(cor);
+        this.raio = raio;
+    }
+    public double getRaio() { return raio; }
+    public void setRaio(double raio) { this.raio = raio; }
+    
+    @Override
+    public double getArea() {
+        return (Math.PI * getRaio() * getRaio()); 
+    }
+}
---
+++ src/model/FiguraGeometrica.java
@@ -0,0 +1,24 @@
+
+package model;
+
+    
+public abstract class FiguraGeometrica {
+    private String cor;
+    public FiguraGeometrica(){
+        this("");
+    }
+
+    public FiguraGeometrica(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public abstract double getArea();
+    
+}
